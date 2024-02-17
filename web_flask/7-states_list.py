c7d499ed5ab5544142562d4e59d6621cff7c0f70
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-17 20:14:46 +0200

    Task 9

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""First Module"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def hello_world():
+    """rendered at path (/) """
+    states = list(storage.all("State").values())
+    print(states)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def on_teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    """main method"""
+    app.run(host="0.0.0.0", port=5000)
