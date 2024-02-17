cfe19c1af63905ba8c7fcbc34ee1edea25cd2ea7
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-18 01:03:27 +0200

    ??!

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def hello_world():
     """rendered at path (/) """
-    states = list(sorted(storage.all("State").values(), key=lambda x: x.name))
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State")
+    return render_template('7-states_list.html', Table="States", states=states)
 
 
 @app.teardown_appcontext
