a702e63ca40d90764ff2bcccefff58cbe2ad9624
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-18 17:08:28 +0200

    Fix??

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
 -- a script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 USE hbnb_dev_db;
