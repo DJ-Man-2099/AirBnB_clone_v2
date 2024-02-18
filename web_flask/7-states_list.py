9424b1b89bf6f29d12211cae9b733d48fd1e2af9
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-18 16:37:22 +0200

    !?!?!

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,50 +7,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """rendered at path (/) """
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hello_world_hbnb():
-    """rendered at path (/hbnb) """
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def hello_world_c_text(text):
-    """rendered at path (/c/<text>) """
-    return f"C {text.replace('_',' ')}"
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def hello_world_number_n(n):
-    """rendered at path (/number/<n>) """
-    return f"{n} is a number"
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def hello_world_number_template_n(n):
-    """rendered at path (/number/<n>) """
-    return render_template('5-number.html', n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def hello_world_number_odd_or_even_n(n):
-    """rendered at path (/number/<n>) """
-    string = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, string=string)
-
-
-@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def hello_world_python_text(text):
-    """rendered at path (/python/<text>) """
-    return f"Python {text.replace('_',' ')}"
-
-
 @app.route("/states_list", strict_slashes=False)
 def hello_world_states_list():
     """rendered at path (/) """
