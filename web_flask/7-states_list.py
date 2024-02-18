2f3747e8fce0fdd7bdb0b02ba08b7fb2ccc54ba8
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-18 19:50:48 +0200

    ??>

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 def hello_world_states_list():
     """rendered at path (/) """
     try:
-        states = sorted(list(
-            storage.all("State").values()), key=lambda x: x.name)
+        states = storage.all("State").values()
     except Exception as e:
         states = []
     return render_template('7-states_list.html', states=states)
